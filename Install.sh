3fe625e8155c3349b66191faf5eb00772b952baa
Mark Nottingham <mnot@mnot.net>
2012-08-29 15:33:17 +1000

    allow specification of a user when installing

--- Install.sh
+++ Install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+user=$1 || $USER
+
 srcs=*
 
 for dotfile in $srcs ; do 
     if [ $dotfile == 'Install.sh' ] ; then
         continue;
     fi
-    cp -vf ${dotfile} ~/.${dotfile};
+    cp -vf ${dotfile} ~${user}/.${dotfile};
+    chown $user ~${user}/.${dotfile};
 done
